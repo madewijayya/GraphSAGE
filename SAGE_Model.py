434a77c78a708d1c4e01219e4cf8ff8d8c85bc7e
madewijayya <90542616+madewijayya@users.noreply.github.com>
2023-12-07 10:44:09 +0700

    Create SAGE_Model.py

---
+++ SAGE_Model.py
@@ -0,0 +1,86 @@
+class sageGNN(layers.Layer):
+    def __init__(self, units, num_task=2, merge_type="concat", **kwargs):
+        super().__init__(**kwargs)
+        self.num_task = num_task
+        self.merge_type = merge_type
+        self.attention_layers = [GraphAttention(units) for _ in range(num_task)]
+
+    def call(self, inputs):
+        atom_features, pair_indices = inputs
+
+        # Obtain outputs from each attention head
+        outputs = [
+            attention_layer([atom_features, pair_indices])
+            for attention_layer in self.attention_layers
+        ]
+        # Concatenate or average the node states from each head
+        if self.merge_type == "concat":
+            outputs = tf.concat(outputs, axis=-1)
+        else:
+            outputs = tf.reduce_mean(tf.stack(outputs, axis=-1), axis=-1)
+        # Activate and return node states
+        return tf.nn.relu(outputs)
+
+
+class GraphNNSage(keras.Model):
+    def __init__(
+        self,
+        node_states,
+        edges,
+        hidden_units,
+        num_heads,
+        num_layers,
+        output_dim,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.node_states = node_states
+        self.edges = edges
+        self.preprocess = layers.Dense(hidden_units * num_heads, activation="relu")
+        self.attention_layers = [
+            sageGNN(hidden_units, num_heads) for _ in range(num_layers)
+        ]
+        self.output_layer = layers.Dense(output_dim)
+
+    def call(self, inputs):
+        node_states, edges = inputs
+        x = self.preprocess(node_states)
+        for attention_layer in self.attention_layers:
+            x = attention_layer([x, edges]) + x
+        outputs = self.output_layer(x)
+        return outputs
+
+    def train_step(self, data):
+        indices, labels = data
+
+        with tf.GradientTape() as tape:
+            # Forward pass
+            outputs = self([self.node_states, self.edges])
+            # Compute loss
+            loss = self.compiled_loss(labels, tf.gather(outputs, indices))
+        # Compute gradients
+        grads = tape.gradient(loss, self.trainable_weights)
+        # Apply gradients (update weights)
+        optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        # Update metric(s)
+        self.compiled_metrics.update_state(labels, tf.gather(outputs, indices))
+
+        return {m.name: m.result() for m in self.metrics}
+
+    def predict_step(self, data):
+        indices = data
+        # Forward pass
+        outputs = self([self.node_states, self.edges])
+        # Compute probabilities
+        return tf.nn.softmax(tf.gather(outputs, indices))
+
+    def test_step(self, data):
+        indices, labels = data
+        # Forward pass
+        outputs = self([self.node_states, self.edges])
+        # Compute loss
+        loss = self.compiled_loss(labels, tf.gather(outputs, indices))
+        # Update metric(s)
+        self.compiled_metrics.update_state(labels, tf.gather(outputs, indices))
+
+        return {m.name: m.result() for m in self.metrics}
